4db7864b7bad91b6cc83c2b2cc84cd3b7a3e7573
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2023-12-06 15:52:40 +0000

    chore: setup

---
+++ src/server.js
@@ -0,0 +1,68 @@
+// Import database and sync tables
+const { sequelize } = require("./db/connection");
+const syncTables = async () => {
+  await sequelize.sync();
+};
+syncTables();
+
+const express = require("express");
+const port = process.env.PORT;
+
+// swagger
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Accountability App Backend",
+      description:
+        "An app for recording and tracking goals with added accountability from friends",
+      contact: {
+        name: "Michael Z",
+      },
+      servers: ["http://localhost"],
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+
+  apis: ["src/user/routes.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+const app = express();
+
+if (process.env.NODE_ENV === "dev") {
+  app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocs, {
+      explorer: true,
+    })
+  );
+}
+
+app.use(express.json());
+
+// Import routes
+
+app.get("/health", (req, res) =>
+  res.status(200).json({ msg: "API is healthy" })
+);
+
+app.listen(port, () => console.log("Server is listening"));
